6f562f54a1124b0aa29bf273bc2c075d7b5a3a7c
Ryan Le <ryanle@live.com>
2024-05-31 13:45:02 -0400

    feat: route highlighting on navbar

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 // Components
 import { Heading } from '@/components/global';
 import {
@@ -25,95 +27,100 @@ import { HiOutlineMail as Email } from 'react-icons/hi';
 import { RouteMenu } from '@/components/navigation';
 import NextLink from 'next/link';
 
+// Utilities
+import { usePathname } from 'next/navigation';
+
+const routes = [
+  {
+    icon: <Community />,
+    text: 'Community',
+    href: '',
+    subroutes: [
+      {
+        icon: <Calendar />,
+        text: 'Events',
+        href: '/events',
+      },
+      {
+        icon: <Gallery />,
+        text: 'Gallery',
+        href: '/gallery',
+      },
+      {
+        icon: <Trophy />,
+        text: 'Awards',
+        href: '/awards',
+      },
+    ],
+  },
+  {
+    icon: <Shirt />,
+    text: 'Merch',
+    href: 'https://ryanmeetup.etsy.com',
+  },
+  {
+    icon: <Handshake />,
+    text: 'Support',
+    href: '',
+    subroutes: [
+      {
+        icon: <Flyer />,
+        text: 'Posters',
+        href: '/posters',
+      },
+      {
+        icon: <Donate />,
+        text: 'Donate',
+        href: '/donate',
+      },
+    ],
+  },
+  {
+    icon: <Map />,
+    text: 'Map',
+    href: '/map',
+  },
+  {
+    icon: <News />,
+    text: 'Press',
+    href: '/press',
+  },
+  {
+    icon: <Email />,
+    text: 'Contact Us',
+    href: '/contact',
+  },
+  {
+    icon: <Socials />,
+    text: 'Social Media',
+    href: '',
+    subroutes: [
+      {
+        icon: <Instagram />,
+        text: 'Instagram',
+        href: 'https://www.instagram.com/ryanmeetup/',
+      },
+      {
+        icon: <Meetup />,
+        text: 'Meetup',
+        href: 'https://www.meetup.com/ryanmeetup/',
+      },
+      {
+        icon: <Discord />,
+        text: 'Discord',
+        href: 'https://discord.gg/HDugzYSHKC',
+      },
+      {
+        icon: <YouTube />,
+        text: 'YouTube',
+        href: 'https://www.youtube.com/@ryanmeetup',
+      },
+    ],
+  },
+];
+
 const Header = () => {
-  const routes = [
-    {
-      icon: <Community />,
-      text: 'Community',
-      href: '',
-      subroutes: [
-        {
-          icon: <Calendar />,
-          text: 'Events',
-          href: '/events',
-        },
-        {
-          icon: <Gallery />,
-          text: 'Gallery',
-          href: '/gallery',
-        },
-        {
-          icon: <Trophy />,
-          text: 'Awards',
-          href: '/awards',
-        },
-      ],
-    },
-    {
-      icon: <Shirt />,
-      text: 'Merch',
-      href: 'https://ryanmeetup.etsy.com',
-    },
-    {
-      icon: <Handshake />,
-      text: 'Support',
-      href: '',
-      subroutes: [
-        {
-          icon: <Flyer />,
-          text: 'Posters',
-          href: '/posters',
-        },
-        {
-          icon: <Donate />,
-          text: 'Donate',
-          href: '/donate',
-        },
-      ],
-    },
-    {
-      icon: <Map />,
-      text: 'Map',
-      href: '/map',
-    },
-    {
-      icon: <News />,
-      text: 'Press',
-      href: '/press',
-    },
-    {
-      icon: <Email />,
-      text: 'Contact Us',
-      href: '/contact',
-    },
-    {
-      icon: <Socials />,
-      text: 'Social Media',
-      href: '',
-      subroutes: [
-        {
-          icon: <Instagram />,
-          text: 'Instagram',
-          href: 'https://www.instagram.com/ryanmeetup/',
-        },
-        {
-          icon: <Meetup />,
-          text: 'Meetup',
-          href: 'https://www.meetup.com/ryanmeetup/',
-        },
-        {
-          icon: <Discord />,
-          text: 'Discord',
-          href: 'https://discord.gg/HDugzYSHKC',
-        },
-        {
-          icon: <YouTube />,
-          text: 'YouTube',
-          href: 'https://www.youtube.com/@ryanmeetup',
-        },
-      ],
-    },
-  ];
+  const pathname = usePathname();
 
   return (
     <header className='flex justify-between items-center py-5 px-4 border-b border-gray-400 dark:border-gray-700 bg-white dark:bg-black sticky relative top-0 right-0 left-0 z-50 lg:px-32 2xl:px-72 3xl:px-[400px] 4xl:px-[650px]'>
@@ -127,7 +134,7 @@ const Header = () => {
         {routes.map((route) => !route.subroutes ? (
           <NextLink
             key={route.text}
-            className='flex items-center font-semibold rounded tracking-wide gap-x-2 px-2 py-1 border border-black transition duration-300 ease-in-out hover:border hover:border-gray-700'
+            className={`${route.href.includes(pathname) && 'bg-gray-900'} flex items-center font-semibold rounded-lg tracking-wide gap-x-2 px-2 py-1 border border-black transition duration-300 ease-in-out hover:border hover:border-gray-700`}
             href={route.href}
           >
             {route.icon} {route.text}
@@ -138,6 +145,7 @@ const Header = () => {
             icon={route.icon}
             title={route.text}
             routes={route.subroutes}
+            pathname={pathname}
           />
         ))}
       </div>
--- components/navigation/RouteMenu.tsx
+++ components/navigation/RouteMenu.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 // Components
 import {
   Transition,
@@ -19,24 +17,33 @@ type RouteMenuProps = {
   icon: ReactNode;
   title: string;
   routes: Route[];
+  pathname: string;
 };
 
 const RouteMenu = (props: RouteMenuProps) => {
-  const { icon, title, routes } = props;
+  const { icon, title, routes, pathname } = props;
+
+  const isSelected = (pathname: string) => {
+    if ((pathname === '/events' || pathname === '/gallery' || pathname === '/awards') && title === 'Community') {
+      return 'bg-gray-900';
+    } else if ((pathname === '/posters' || pathname === '/donate') && title === 'Support') {
+      return 'bg-gray-900';
+    }
+  };
 
   return (
     <Popover className='relative'>
-      <PopoverButton className='flex items-center font-semibold rounded tracking-wide gap-x-2 px-2 py-1 border border-black transition duration-300 ease-in-out hover:border hover:border-gray-700'>
+      <PopoverButton className={`${isSelected(pathname)} flex items-center font-semibold rounded-lg tracking-wide gap-x-2 px-2 py-1 border border-black transition duration-300 ease-in-out hover:border hover:border-gray-700`}>
         {icon} {title} <ChevronDown className='h-3 w-3 fill-gray-600' />
       </PopoverButton>
 
       <Transition
-        enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
+        enter='transition ease-out duration-200'
+        enterFrom='opacity-0 translate-y-1'
+        enterTo='opacity-100 translate-y-0'
+        leave='transition ease-in duration-150'
+        leaveFrom='opacity-100 translate-y-0'
+        leaveTo='opacity-0 translate-y-1'
       >
         <PopoverPanel
           className='z-50 bg-black w-40 rounded-lg border border-gray-700 p-2 mt-2'
