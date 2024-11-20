7a28ad44a8de2cb9008801d738bc8ef7cab4c4b2
Ryan Le <ryanle@live.com>
2024-11-20 14:58:57 -0500

    chore: update about copy, add end date to banner

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,7 @@
 // Components
 import { Layout } from '@/components/navigation';
-import { Heading, Text, List, Divider } from '@/components/global';
-import { FaHandshake as Handshake } from 'react-icons/fa';
+import { Heading, Text, Divider } from '@/components/global';
 import NextImage from 'next/image';
-import NextLink from 'next/link';
 
 // Types
 import type { Metadata } from 'next';
@@ -33,29 +31,22 @@ export const metadata: Metadata = {
 };
 
 const AboutPage = () => {
-  const topics = [
-    { main: 'What kind of Ryans were they?' },
-    { main: 'How did each of them end up here?' },
-    { main: 'Should they establish a social media presence?' },
-    { main: 'And most importantly, what would come next?' }
-  ];
-
   return (
     <Layout className='space-y-6'>
       <Heading>About Us</Heading>
 
       <div className='grid grid-cols-12 xl:gap-10'>
-        <div className='col-span-12 xl:col-span-6 space-y-6'>
+        <div className='col-span-12 xl:col-span-5 space-y-6'>
           <Text>
-            What began as a simple idea took root when Ryan set out to find other Ryans in her Brooklyn neighborhood. She posted flyers throughout the area, inviting everyone named &quot;Ryan&quot; to join a new community—the Ryan Meetup.
+            In February 2023, Ryan decided she wanted to meet more people named Ryan. So she posted a handful of flyers in her Brooklyn neighborhood with a QR code that implored Ryans to RSVP for a &quot;Ryan Meetup.&quot; Only two other Ryans attended.
           </Text>
 
           <Text>
-            After two weeks of spreading the word, five Ryans had RSVP&apos;d to the inaugural event, dubbed the &quot;Ryan Kickoff&quot; in Brooklyn, NY. With modest attendance expected, Ryan and a fellow Ryan debated whether to proceed. In the end, they decided that even a small gathering could mark the beginning of something special.
+            That night, after checking each other&apos;s IDs, Ryan, Ryan, and Ryan devised a plan to reach more Ryans. And set an ambitious goal for themselves: to break the world record for largest same first name gathering.
           </Text>
         </div>
 
-        <div className='col-span-12 xl:col-span-6 relative mb-4 w-full h-32 h-32 md:h-64 xl:w-full'>
+        <div className='col-span-12 xl:col-span-7 relative mb-4 w-full h-32 h-32 md:h-72 xl:w-full'>
           <NextImage
             className='rounded-xl shadow-xl mt-4'
             fill
@@ -67,28 +58,15 @@ const AboutPage = () => {
       </div>
 
       <Text>
-        On the morning of the event, Ryan also RSVP&apos;d to confirm his attendance. That evening, the three pioneering Ryans gathered at a local Brooklyn bar for their first meeting. Amidst introductions and laughs, they discussed the pressing topics at hand:
-      </Text>
-
-      <List
-        icon={<Handshake className='fill-gray-600 dark:fill-white mt-1 mr-4 w-5 h-5 flex-shrink-0' />}
-        content={topics}
-      />
-
-      <Text>
-        It was during this inaugural event that they collectively decided to keep the momentum going. The next meetup would be titled the &quot;Ryan Roundup&quot; and was set for a new venue in Manhattan, appropriately held at Ryan Maguire&apos;s Ale House.
+        So the Ryans got to work, covering NYC in flyers to advertise their next event at Ryan Maguire&apos;s pub—but this time, over 100 Ryans showed up. Including some Ryans from major news sources, who helped spread the word to even more Ryans.
       </Text>
 
       <Text>
-        Determined to broaden their reach, each Ryan began promoting the Roundup with new flyers posted around their own parts of the city. Soon, &quot;Ryan Meetup&quot; notices started popping up in neighborhoods across New York—from Bushwick and Citi Field to Hudson Yards, Hell&apos;s Kitchen, and Flatiron. Eventually, the movement expanded across the country, with flyers appearing in Austin, TX, Myrtle Beach, SC, and Washington, DC.
+        Soon, Ryans everywhere wanted to join in on the fun. So we took it on tour, hosting Ryan Meetups in Los Angeles, Austin, Chicago, Miami, and Toronto. All while building a community with thousands of Ryans, who each contribute in their own unique way. And that&apos;s exactly what it will take to put our name in the record books.
       </Text>
 
       <Text>
-        The Roundup quickly gained traction online, catching the eye of <NextLink className='text-blue-500 hover:underline' href='https://www.instagram.com/whatisnewyork/?hl=en'>@WhatIsNewYork on Instagram</NextLink>, making it onto LIVE with Kelly and Ryan, and even reaching the front page of Reddit. By the time the event rolled around, over 100 Ryans attended, attracting attention from major outlets like the New York Times, NPR, and beyond.
-      </Text>
-
-      <Text>
-        With this growing recognition, it was clear: the Ryan Meetup was only beginning.
+        When your name is called, be ready Ryan. We&apos;re going to need you.
       </Text>
 
       <Divider />
