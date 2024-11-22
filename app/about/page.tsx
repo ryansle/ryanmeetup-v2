9e3ec0ddd18f115db038bcf369193b3f7e37ef70
Ryan Le <ryanle@live.com>
2024-11-22 14:50:41 -0500

    fix: adjust about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -32,44 +32,40 @@ export const metadata: Metadata = {
 
 const AboutPage = () => {
   return (
-    <Layout className='space-y-6'>
-      <Heading>About Us</Heading>
-
-      <div className='grid grid-cols-12 xl:gap-10'>
-        <div className='col-span-12 xl:col-span-5 space-y-6'>
-          <Text>
-            In February 2023, Ryan decided she wanted to meet more people named Ryan. So she posted a handful of flyers in her Brooklyn neighborhood with a QR code that implored Ryans to RSVP for a &quot;Ryan Meetup.&quot; Only two other Ryans attended.
-          </Text>
-
-          <Text>
-            That night, after checking each other&apos;s IDs, Ryan, Ryan, and Ryan devised a plan to reach more Ryans. And set an ambitious goal for themselves: to break the world record for largest same first name gathering.
-          </Text>
-        </div>
-
-        <div className='col-span-12 xl:col-span-7 relative mb-4 w-full h-32 h-32 md:h-72 xl:w-full'>
+    <Layout>
+      <div className='xl:px-20 space-y-6'>
+        <div className='relative mb-10 w-full h-48 md:h-80 lg:h-72 xl:h-[440px]'>
           <NextImage
-            className='rounded-xl shadow-xl mt-4'
+            className='rounded-xl shadow-xl mt-4 lg:object-center xl:object-top'
             fill
             src='/ryankickoff.png'
             objectFit='cover'
             alt='The first Ryan Meetup'
           />
         </div>
+        <Heading>About Us</Heading>
+
+        <Text>
+          In February 2023, Ryan decided she wanted to meet more people named Ryan. So she posted a handful of flyers in her Brooklyn neighborhood with a QR code that implored Ryans to RSVP for a &quot;Ryan Meetup.&quot; Only two other Ryans attended.
+        </Text>
+
+        <Text>
+          That night, after checking each other&apos;s IDs, Ryan, Ryan, and Ryan devised a plan to reach more Ryans. And set an ambitious goal for themselves: to break the world record for largest same first name gathering.
+        </Text>
+
+        <Text>
+          So the Ryans got to work, covering NYC in flyers to advertise their next event at Ryan Maguire&apos;s pub—but this time, over 100 Ryans showed up. Including some Ryans from major news sources, who helped spread the word to even more Ryans.
+        </Text>
+
+        <Text>
+          Soon, Ryans everywhere wanted to join in on the fun. So we took it on tour, hosting Ryan Meetups in Los Angeles, Austin, Chicago, Miami, and Toronto. All while building a community with thousands of Ryans, who each contribute in their own unique way. And that&apos;s exactly what it will take to put our name in the record books.
+        </Text>
+
+        <Text>
+          When your name is called, be ready Ryan. We&apos;re going to need you.
+        </Text>
       </div>
-
-      <Text>
-        So the Ryans got to work, covering NYC in flyers to advertise their next event at Ryan Maguire&apos;s pub—but this time, over 100 Ryans showed up. Including some Ryans from major news sources, who helped spread the word to even more Ryans.
-      </Text>
-
-      <Text>
-        Soon, Ryans everywhere wanted to join in on the fun. So we took it on tour, hosting Ryan Meetups in Los Angeles, Austin, Chicago, Miami, and Toronto. All while building a community with thousands of Ryans, who each contribute in their own unique way. And that&apos;s exactly what it will take to put our name in the record books.
-      </Text>
-
-      <Text>
-        When your name is called, be ready Ryan. We&apos;re going to need you.
-      </Text>
-
-      <Divider />
+      {/* <Divider />
 
       <div className='grid grid-cols-1 gap-x-4 gap-y-4 md:grid-cols-2 xl:grid-cols-3'>
         {gallery.map((photo, index) => (
@@ -93,7 +89,7 @@ const AboutPage = () => {
             </Text>
           </div>
         ))}
-      </div>
+      </div> */}
     </Layout>
   );
 };
