4a8d699ab9ea750a2fb4c6263bfaff2753ed55d9
Ryan Le <ryanle@ryans-mbp.mynetworksettings.com>
2025-04-21 18:20:18 -0400

    chore: add discord shortcut

---
+++ app/discord/page.tsx
@@ -0,0 +1,31 @@
+// Types
+import type { Metadata } from 'next';
+
+// Utilities
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: 'Ryan Meetup - Discord',
+  description: 'Join the Ryan Meetup Discord server!',
+  openGraph: {
+    url: 'https://ryanmeetup.com/discord',
+    title: 'Ryan Meetup - Discord',
+    description: 'Join the Ryan Meetup Discord server!',
+    siteName: 'Ryan Meetup',
+    images: [
+      {
+        url: 'https://ryanmeetup.com/ryankickoff.png',
+        width: 1600,
+        height: 800,
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+};
+
+const DiscordPageRedirect = () => {
+  redirect('https://discord.gg/8rPPQMtZCp');
+};
+
+export default DiscordPageRedirect;
