2a3e307e57473087d71650bdeba097c7b51dbed2
Ryan Le <ryanle@live.com>
2025-04-29 16:42:44 -0400

    feat: update rsvp page

--- app/rsvp/page.tsx
+++ app/rsvp/page.tsx
@@ -1,23 +1,28 @@
+// Components
+import { Layout } from '@/components/navigation';
+import { Heading } from '@/components/global';
+import { DoubleHeader } from '@/components/events/DoubleHeader';
 
 // Types
 import type { Metadata } from 'next';
+import type { RyanEvent } from '@/lib/types';
 
 // Utilities
-import { redirect } from 'next/navigation';
+import { fetchEvents } from '@/actions/fetchContent';
 
 export const metadata: Metadata = {
   title: 'Ryan Meetup - RSVP',
-  description: 'RSVP to St. Ryan\'s Day II in Boston, MA, on March 22nd.',
+  description: 'RSVP to Ryans at the Rockies and the Ryan Summit in Denver, CO, on June 20 + 21st.',
   openGraph: {
     url: 'https://ryanmeetup.com/rsvp',
     title: 'Ryan Meetup - RSVP',
-    description: 'RSVP to St. Ryan\'s Day II in Boston, MA, on March 22nd.',
+    description: 'RSVP to Ryans at the Rockies and the Ryan Summit in Denver, CO, on June 20 + 21st.',
     siteName: 'Ryan Meetup',
     images: [
       {
-        url: 'https://ryanmeetup.com/logos/stryan2.png',
-        width: 2400,
-        height: 1350,
+        url: 'https://ryanmeetup.com/logos/doubleheader.png',
+        width: 1920,
+        height: 1080,
       }
     ],
     locale: 'en_US',
@@ -26,7 +31,16 @@ export const metadata: Metadata = {
 };
 
 const RSVPPage = async () => {
-  redirect('https://www.meetup.com/ryanmeetup/events/306368238/');
+  const events = await fetchEvents();
+
+  return (
+    <Layout>
+      <Heading className='mb-2 text-center'>One Weekend. Back-to-back Ryan Meetups.</Heading>
+      <Heading size='md' className='mb-6 text-center'>...in a brand new city!</Heading>
+
+      <DoubleHeader events={events as RyanEvent[]} />
+    </Layout>
+  );
 };
 
 export default RSVPPage;
--- components/navigation/Banner.tsx
+++ components/navigation/Banner.tsx
@@ -5,8 +5,8 @@ import NextLink from 'next/link';
 
 const Banner = () => {
   return (
-    <div className='font-cooper text-white bg-green-600 py-1 px-4 lg:px-32 2xl:px-56 3xl:px-[350px] 4xl:px-[500px]'>
-      St. Ryan&apos;s Day returns. Join us in Boston, MA, on March 22nd, 2025. <NextLink href='/rsvp' className='underline'>RSVP today</NextLink>.
+    <div className='font-cooper text-white bg-[#7e6a8f] py-1 px-4 lg:px-32 2xl:px-56 3xl:px-[350px] 4xl:px-[500px]'>
+      Denver Ryans asked. We answered. We&apos;ll see you there for our next double header, Ryans. <NextLink href='/rsvp' className='underline'>RSVP today</NextLink>.
     </div>
   );
 };
