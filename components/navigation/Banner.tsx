a9d8756ac3964b686ea811934abb7eeed58e76d4
Ryan Le <ryanle@live.com>
2026-01-15 13:50:28 -0500

    feat: chapter banner

--- components/navigation/Banner.tsx
+++ components/navigation/Banner.tsx
@@ -5,12 +5,12 @@ import NextLink from "next/link";
 
 const Banner = () => {
   return (
-    <div className="font-cooper text-white bg-[#1d1b7a] py-1 px-4 lg:px-32 2xl:px-56 3xl:px-[350px] 4xl:px-[500px]">
-      Join us to show the world why Ryans Own Manhattan at the final Ryan meetup of 2025. {' '}
-      <NextLink href="/rsvp" className="underline">
-        RSVP today
+    <div className="font-cooper text-white bg-red-500 py-1 px-4 lg:px-32 2xl:px-56 3xl:px-[350px] 4xl:px-[500px]">
+      Ryan Meetup is expanding across North America! Find a local chapter near you at{" "}
+      <NextLink href="/chapters" className="underline hover:text-blue-300">
+        ryanmeetup.com/chapters
       </NextLink>
-      .
+      {" "}.
     </div>
   );
 };
